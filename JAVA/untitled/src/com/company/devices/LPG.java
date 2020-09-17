53cf80c9ef89e4c499829787417e0cd46d40585c
Krzykrzychu2020 <64839677+Krzykrzychu2020@users.noreply.github.com>
2020-09-18 01:36:35 +0200

    Update LPG.java

--- JAVA/untitled/src/com/company/devices/LPG.java
+++ JAVA/untitled/src/com/company/devices/LPG.java
@@ -3,6 +3,6 @@ package com.company.devices;
 public class LPG extends Car {
     @Override
     public String refuel() {
-        return "potrzeba Lepszego powera Gamoniu (LPG)";
+        return "LPG";
     }
 }
