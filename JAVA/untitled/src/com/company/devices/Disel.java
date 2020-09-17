78e4be2ec55795e0124d8201d604e1348c1e54b9
Krzykrzychu2020 <64839677+Krzykrzychu2020@users.noreply.github.com>
2020-09-18 01:36:10 +0200

    Update Disel.java

--- JAVA/untitled/src/com/company/devices/Disel.java
+++ JAVA/untitled/src/com/company/devices/Disel.java
@@ -3,6 +3,6 @@ package com.company.devices;
 public class Disel extends Car {
     @Override
     public String refuel() {
-        return "zalej ekodizelka byku ";
+        return "dolej disiela;
     }
 }
